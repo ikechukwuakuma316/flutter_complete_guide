9b06675632f221804a09ccdb6e2bac5801ec34a9
savvy316 <ikechukwuakuma316@gmail.com>
2022-04-27 22:38:26 +0100

    raised button adjusted

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answerchosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
